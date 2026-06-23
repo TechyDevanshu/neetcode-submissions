67b4a7fe8782b7b5deb01622ebe4b90fcf7cdf8e
Devanshu Urmaliya <devanshuurmaliya@gmail.com>
2026-06-23 23:26:11 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        int []freq= new int[26];
+        for(char ch:tasks){
+            freq[ch-'A']++;
+        }
+
+        PriorityQueue<Integer>maxHeap= new PriorityQueue<>((a,b)->b-a);
+
+        for(int f:freq){
+            if(f>0){
+                maxHeap.add(f);
+            }
+        }
+
+        int time=0;
+
+        while(!maxHeap.isEmpty()){
+
+            int cycles=n+1;
+
+            List<Integer>temp= new ArrayList<>();
+
+            while(cycles>0 && !maxHeap.isEmpty()){
+                int top=maxHeap.poll();
+                if(top-1>0){
+                    temp.add(top-1);
+                }
+                cycles--;
+                time++;
+            }
+            maxHeap.addAll(temp);
+
+            if(!maxHeap.isEmpty()){
+                time+=cycles;
+            }
+        }
+        return time;
+    }
+}
